cb2c971c8858d325acfa6c55cf141fb72ed32b15
shafin4201 <arinisshafin@gmail.com>
2025-02-12 17:54:26 +0600

    Update audio_stream.py

--- services/audio_stream.py
+++ services/audio_stream.py
@@ -1,1 +1,11 @@
-Nothing 
+import subprocess
+
+def start_audio_stream():
+    return subprocess.Popen(
+        ["ffmpeg", "-f", "alsa", "-i", "default", "-acodec", "libmp3lame", "-f", "mp3", "pipe:1"],
+        stdout=subprocess.PIPE, stderr=subprocess.DEVNULL
+    )
+
+def stop_audio_stream(process):
+    if process:
+        process.terminate()
