2994617ba9eab6482e76abd51dc04d826296a9c2
shafin4201 <arinisshafin@gmail.com>
2025-02-13 18:12:00 +0600

    Update main.py

--- main.py
+++ main.py
@@ -79,4 +79,4 @@ if __name__ == '__main__':
     keep_alive_thread.start()
 
     port = int(os.environ.get("PORT", 10000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port, debug=True)
